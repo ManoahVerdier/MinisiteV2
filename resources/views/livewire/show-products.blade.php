b029af78373db8b926ef8f0361e66e41d82c4ac7
Manoah Verdier <verdier.developpement@gmail.com>
2022-08-31 17:10:24 +0200

    li

--- resources/views/livewire/show-products.blade.php
+++ resources/views/livewire/show-products.blade.php
@@ -302,7 +302,7 @@
             </div>
           </div>
           <div class="w-100">
-            <a href="#" class=" mx-auto btn btn-relief-primary {{ $compared?"bg-light-primary text-primary":"" }} btn-comparison mb-md-1"  product-id="{{ $product->id }}">
+            <a href="#" class=" mx-auto btn btn-relief-primary {{ $compared?"bg-light-primary text-primary":"" }} btn-comparison mb-1"  product-id="{{ $product->id }}">
               <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="{{ $compared?"text-primary":"text-white" }} feather feather-award"><circle cx="12" cy="8" r="7"></circle><polyline points="8.21 13.89 7 23 12 20 17 23 15.79 13.88"></polyline></svg>
               <span>{{ $compared?"Comparé":"Comparer" }}</span>
             </a>
