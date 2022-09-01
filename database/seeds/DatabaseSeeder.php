67af15ff86b52fbe68ded6939762576d923d72ea
Manoah Verdier <verdier.developpement@gmail.com>
2022-09-01 08:10:20 +0200

    seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
         $this->call(MenusTableSeeder::class);
         $this->call(MenuItemsTableSeeder::class);
         
@@ -22,8 +21,8 @@ class DatabaseSeeder extends Seeder
         
         $this->call(PagesTableSeeder::class);
         $this->call(RolesTableSeeder::class);
-        $this->call(PermissionsTableSeeder::class);
-        $this->call(PermissionRoleTableSeeder::class);
+//        $this->call(PermissionsTableSeeder::class);
+//        $this->call(PermissionRoleTableSeeder::class);
         
         
         $this->call(SettingsTableSeeder::class);
