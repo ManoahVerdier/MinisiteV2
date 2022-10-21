d90ec97c11d558bbdd9f458dcc432d4f3d39c2ff
Manoah Verdier <verdier.developpement@gmail.com>
2022-10-21 10:45:31 +0200

    genode

--- getNode.sh
+++ getNode.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PAST=$(pwd)
 cd ~
-curl https://nodejs.org/dist/latest/node-v18.9.0-linux-x64.tar.gz | tar xz
+curl https://nodejs.org/dist/v18.9.0/node-v18.9.0-linux-x64.tar.gz | tar xz
 mv node-v18.9.0-linux-x64/ nodejs
 mkdir ~/bin
 cp nodejs/bin/node ~/bin
@@ -10,7 +10,7 @@ rm npm
 ln -s ../nodejs/lib/node_modules/npm/bin/npm-cli.js npm
 cd ..
 source ~/.nvm/nvm.sh
-nvm install 10.24.1
+nvm install 18.9.0
 source ~/.nvm/nvm.sh
 nvm use --delete-prefix v10.24.1
 cd $PAST
